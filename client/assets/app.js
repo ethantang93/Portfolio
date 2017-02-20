02ed07204283f24f01aa30a0b862297e95bfd44d
ethantang93 <tangrenxing@gmail.com>
2017-02-20 16:52:53 -0600

    reduced files size

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,16 +1,4 @@
 var app = angular.module('app',['ngMaterial']);
-// app.config(function($routeProvider){
-//   $routeProvider.when('root',{
-//     templateUrl:"/partials/partial1.html"
-//   })
-//   .when('/partial2',{
-//     templateUrl:"/partials/partial2.html"
-//   })
-//   .when('/partial3',{
-//     templateUrl:"/partials/partial3.html"
-//   })
-//   .otherwise({redirectTo:'root'})
-// });
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('red')
