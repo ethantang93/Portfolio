cd9545d534643b03f53c648a0fc7939e2ec1332c
ethantang93 <tangrenxing@gmail.com>
2017-02-28 18:07:21 -0600

    optimization

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,13 +1,1 @@
-var app = angular.module('app',['ngMaterial']);
-app.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('red')
-    .accentPalette('green')
-    .warnPalette('blue');
-});
-app.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
-});
+var app = angular.module('app',[]);
--- client/assets/controllers/Controller1.js
+++ client/assets/controllers/Controller1.js
@@ -1,14 +1,3 @@
-app.controller('myController',['$scope','$mdDialog',function($scope,$mdDialog){
-  $scope.showDetail = function(ev){
-    $mdDialog.show(
-      $mdDialog.alert()
-      .parent(angular.element(document.querySelector('#popUpContainer1')))
-      .clickOutsideToClose(true)
-      .title("Details")
-      .textContent(" User can log in  and list their product for sale and other users can buy products thats avaliable. Technologies used: MongoDB, Express.js, Angular.js, Node.js")
-      .ariaLabel('Alert Dialog Demo')
-      .ok('Close')
-      .targetEvent(ev)
-    )
-  }
+app.controller('myController',['$scope',function($scope){
+
 }])
